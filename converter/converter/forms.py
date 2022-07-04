c807b585ff8ddd766ddbe9d160ca18ed32716b64
Lev Yuvensky <levayu22@gmail.com>
2022-07-04 10:55:11 +0300

    Add sending a list of currencies to the form

--- converter/converter/forms.py
+++ converter/converter/forms.py
@@ -3,5 +3,12 @@ from django import forms
 
 class ExchangeForm(forms.Form):
     amount = forms.IntegerField()
-    from_currency = forms.Select()
-    to_currency = forms.Select()
+    from_currency = forms.ChoiceField()
+    to_currency = forms.ChoiceField()
+
+    def __init__(self, *args, **kwargs):
+        currencies_list = kwargs.pop('currencies')
+        self.currencies = tuple(zip(currencies_list, currencies_list))
+        super(ExchangeForm, self).__init__(*args, **kwargs)
+        self.fields['to_currency'].choices = self.currencies
+        self.fields['from_currency'].choices = self.currencies
