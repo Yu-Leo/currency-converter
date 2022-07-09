d8e7b1afe74944c9dafa839af4c58cf33ade252e
Lev Yuvenskiy <levayu22@gmail.com>
2022-07-09 12:39:57 +0300

    Fix bug with redis db index

--- converter/converter/services/settings.py
+++ converter/converter/services/settings.py
@@ -3,7 +3,7 @@ import os
 # Redis settings
 REDIS_HOST = os.getenv('REDIS_HOST')
 REDIS_PORT = int(os.getenv('REDIS_PORT'))
-REDIS_DB = int(os.getenv('REDIS_PORT', default='0'))
+REDIS_DB = int(os.getenv('REDIS_DB', default='0'))
 
 # Exchange rate API:
 EXCHANGE_RATE_API_URL = 'https://api.exchangerate-api.com/v4/latest/USD'
