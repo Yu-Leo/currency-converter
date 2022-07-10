cfb06a5cf6b0d7a6c180ccc0961648c8e0c05265
Lev Yuvensky <levayu22@gmail.com>
2022-07-10 08:53:55 +0300

    Edit conversion mechanism

--- converter/converter/services/services.py
+++ converter/converter/services/services.py
@@ -1,7 +1,5 @@
 from typing import NamedTuple
 
-import requests
-
 from . import exceptions
 from . import settings
 from .ExchangeRateAPI import ExchangeRateAPI
@@ -31,29 +29,31 @@ def get_currencies_list() -> list[str]:
 
 
 def convert(operation: Operation) -> float:
-    currencies_values = _get_currencies_values()
-    return _calculate(operation.amount, operation.primary_currency,
-                      operation.secondary_currency, currencies_values)
+    primary_currency_value, secondary_currency_value = _get_currencies_values(operation.primary_currency,
+                                                                              operation.secondary_currency)
+    return _calculate(operation.amount, primary_currency_value, secondary_currency_value)
 
 
-def _get_currencies_values() -> dict[str, float]:
-    """
-    :return: dictionary with currencies from API.
-    Key - name of currency
-    Value - the cost of one dollar (USD) in the currency
-    """
-    try:
-        resource = requests.get(url=settings.EXCHANGE_RATE_API_URL).json()
-        return resource.get('rates')
-    except:
-        raise exceptions.APIException
+def _get_currencies_values(primary_currency, secondary_currency) -> tuple[float, float]:
+    database: IDatabase = RedisDatabase(host=settings.REDIS_HOST,
+                                        port=settings.REDIS_PORT,
+                                        db=settings.REDIS_DB)
+
+    primary_currency_value = database.get_currency_value(primary_currency)
+    secondary_currency_value = database.get_currency_value(secondary_currency)
+    return primary_currency_value, secondary_currency_value
+    # try:
+    #     resource = requests.get(url=settings.EXCHANGE_RATE_API_URL).json()
+    #     return resource.get('rates')
+    # except:
+    #     raise exceptions.APIException
 
 
-def _calculate(amount: float, primary_currency: str, secondary_currency: str, currencies: dict[str, float]) -> float:
+def _calculate(amount: float, primary_currency_value: float, secondary_currency_values: float) -> float:
     """
     :param amount: amount to be converted
     """
     try:
-        return round((currencies[secondary_currency] / currencies[primary_currency]) * amount, 6)
+        return round((secondary_currency_values / primary_currency_value) * amount, 6)
     except ZeroDivisionError:
         raise exceptions.ExchangeRateException
