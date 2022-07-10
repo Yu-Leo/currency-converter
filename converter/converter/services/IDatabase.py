a0f7e3fb1b787ffb203a6044a6cec70a7a5b629b
Lev Yuvensky <levayu22@gmail.com>
2022-07-10 08:34:02 +0300

    Rename database methods

--- converter/converter/services/IDatabase.py
+++ converter/converter/services/IDatabase.py
@@ -9,7 +9,7 @@ class IDatabase(ABC):
     # Currencies values
 
     @abstractmethod
-    def set_value(self, key: str, value: float) -> None:
+    def set_currency_value(self, key: str, value: float) -> None:
         """
         :param key: name of currency
         :param value: the cost of one dollar (USD) in the currency
@@ -17,7 +17,7 @@ class IDatabase(ABC):
         pass
 
     @abstractmethod
-    def set_values(self, values: dict[str, float]) -> None:
+    def set_currency_values(self, values: dict[str, float]) -> None:
         """
         :param values: dict with pairs:
         name of currency - the cost of one dollar (USD) in the currency
@@ -25,7 +25,7 @@ class IDatabase(ABC):
         pass
 
     @abstractmethod
-    def get_value(self, key: str) -> float:
+    def get_currency_value(self, key: str) -> float:
         """
         :param key: name of currency
         :return: the cost of one dollar (USD) in the currency
@@ -35,18 +35,18 @@ class IDatabase(ABC):
     # Currencies list
 
     @abstractmethod
-    def set_list(self, currencies_list: list[str]) -> None:
+    def set_currencies_list(self, currencies_list: list[str]) -> None:
         """
         :param currencies_list: list with names of currencies
         """
         pass
 
     @abstractmethod
-    def is_list_exists(self) -> bool:
+    def is_currencies_list_exists(self) -> bool:
         pass
 
     @abstractmethod
-    def get_list(self) -> list[str]:
+    def get_currencies_list(self) -> list[str]:
         """
         :return: list with names of currencies
         """
